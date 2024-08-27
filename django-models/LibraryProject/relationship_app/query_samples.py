b3156b98a4ede7590a8bde826ae8b3e9d6347810
Amos Omariba <amomariba@gmail.com>
2024-08-27 14:07:56 +0300

    created app

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -1,20 +1,26 @@
-# Import necessary models
-from .models import Library, Author, Librarian, Book
+from relationship_app.models import Author, Book, Library, Librarian
 
-# Fetch all books by a specific author
-books = Book.objects.filter(author__name='Example')
+def query_books_by_author(author_name):
+    author = Author.objects.get(name=author_name)
+    books = Book.objects.filter(author=author)
+    return books
 
-# Fetch a specific library by name
-library = Library.objects.get(name='library_name')
+def list_books_in_library(library_name):
+    library = Library.objects.get(name=library_name)
+    return library.books.all()
 
-# Fetch all books in the specified library
-library_books = Book.objects.filter(library=library)
+def get_librarian_for_library(library_name):
+    library = Library.objects.get(name=library_name)
+    return Librarian.objects.get(library=library)
 
-# Fetch a specific author by name
-author = Author.objects.get(name='author_name')
+# Example usage:
+if __name__ == "__main__":
+    # Assuming you've already created some sample data
+    author_books = query_books_by_author("J.K. Rowling")
+    print("Books by J.K. Rowling:", [book.title for book in author_books])
 
-# Fetch all books by the specific author
-author_books = Book.objects.filter(author=author)
+    library_books = list_books_in_library("New York Public Library")
+    print("Books in New York Public Library:", [book.title for book in library_books])
 
-# Fetch a librarian for a specific library
-librarian = Librarian.objects.get(library=library)
+    librarian = get_librarian_for_library("New York Public Library")
+    print("Librarian of New York Public Library:", librarian.name)
