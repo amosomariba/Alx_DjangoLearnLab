072b78dfee543d91384c2a119ccdb8efdf3f9edf
Amos Omariba <amomariba@gmail.com>
2024-09-18 15:58:58 +0300

    Project Setup and User Authentication for a Social Media API

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -1,43 +1,53 @@
+from django.contrib.auth import get_user_model
 from rest_framework import serializers
 from rest_framework.authtoken.models import Token
-from django.contrib.auth import get_user_model
+from django.contrib.auth import authenticate
+from rest_framework.exceptions import AuthenticationFailed
 
-User = get_user_model()
 
 class RegisterSerializer(serializers.ModelSerializer):
+    # Password field as a CharField with write-only
     password = serializers.CharField(write_only=True)
-
+    
     class Meta:
-        model = User
-        fields = ('username', 'password', 'email')
+        model = get_user_model()
+        fields = ('username', 'email', 'password')
 
+    # Overriding the create method to use create_user
     def create(self, validated_data):
-        user = User.objects.create_user(
+        user = get_user_model().objects.create_user(
             username=validated_data['username'],
             password=validated_data['password'],
             email=validated_data['email']
         )
-        # Create an authentication token for the user
-        Token.objects.create(user=user)
         return user
-
-from rest_framework import serializers
-from django.contrib.auth import get_user_model
-
-User = get_user_model()
 
 class RegisterSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True)
-
+    
     class Meta:
-        model = User
-        fields = ('username', 'password', 'email')
+        model = get_user_model()
+        fields = ('username', 'email', 'password')
 
     def create(self, validated_data):
-        # Creating a new user using the 'create_user' method, which automatically hashes the password
-        user = User.objects.create_user(
+        user = get_user_model().objects.create_user(
             username=validated_data['username'],
             password=validated_data['password'],
-            email=validated_data.get('email')
+            email=validated_data['email']
         )
+        # Create a token for the new user
+        Token.objects.create(user=user)
         return user
+
+class LoginSerializer(serializers.Serializer):
+    username = serializers.CharField()
+    password = serializers.CharField(write_only=True)
+
+    def validate(self, data):
+        user = authenticate(username=data['username'], password=data['password'])
+        if not user:
+            raise AuthenticationFailed('Invalid credentials')
+
+        # Retrieve the token for the user
+        token, created = Token.objects.get_or_create(user=user)
+        return {'token': token.key}
