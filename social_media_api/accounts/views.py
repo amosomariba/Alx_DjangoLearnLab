cc9df3b3ed15f58ff1f0dd67f4d9f1efc0dcbf0b
Amos Omariba <amomariba@gmail.com>
2024-09-18 16:04:19 +0300

    Project Setup and User Authentication for a Social Media API

--- social_media_api/accounts/views.py
+++ social_media_api/accounts/views.py
@@ -7,6 +7,10 @@ from rest_framework.views import APIView
 from django.contrib.auth import get_user_model
 from .serializers import RegisterSerializer
 
+from rest_framework.response import Response
+from rest_framework import status
+from .serializers import LoginSerializer
+
 User = get_user_model()
 
 class RegisterView(APIView):
@@ -19,4 +23,11 @@ class RegisterView(APIView):
                 'refresh': str(refresh),
                 'access': str(refresh.access_token),
             }, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class LoginView(APIView):
+    def post(self, request):
+        serializer = LoginSerializer(data=request.data)
+        if serializer.is_valid():
+            return Response(serializer.validated_data, status=status.HTTP_200_OK)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
