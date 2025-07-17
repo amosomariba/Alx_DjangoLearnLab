ee5cbf9c1d64feb234f867865e35b624db8fcd7e
Amos Omariba <amomariba@gmail.com>
2025-07-17 19:17:18 +0300

    Django Views and URL Configuration

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.views.generic import TemplateView, ListView, DetailView
 
-from .models import Book, Library
+from .models import Book
 from .models import  Library
 
 
